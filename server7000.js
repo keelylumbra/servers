6c498859321fd5b0f8a073cfbea303632a3bd5dc
Keely <keelylumbra@knights.ucf.edu>
2016-09-17 11:06:46 -0400

    fixed random

--- server7000.js
+++ server7000.js
@@ -5,15 +5,17 @@ var PORT = 7000;
 
 
 var positives = ["That boob job almost looks real!", "I bet you go to the dentist more than bob does", "You dont look like you ate at golden corral last night!"]
-var negatives = ["You got hit with the ugly stick real hard.", "I didnt realize that wasn't a mask!", "Whose your plastic surgeon? I just want to make sure, I don't use him."]
+var negatives = ["You got hit with the ugly stick real hard.", "I didnt realize that wasn't a mask!", "Whose your plastic surgeon? I just want to make sure, I don't use him.", "You look like a douchebag. And not a cute one."]
 
 
-var randomNice = positives[Math.floor(positives.length * Math.random())];
-var randomMean = negatives[Math.floor(negatives.length * Math.random())];
+
 
 var server = http.createServer(function (request, response){
 	response.statusCode = 503;
-	response.end (randomNice + '\n Path Hit: ' + request.url);
+	var randomNice = positives[Math.floor(positives.length * Math.random())];
+	response.end (
+
+		(randomNice + '\n Path Hit: ' + request.url));
 
 });
 
@@ -28,7 +30,11 @@ var PORT2 = 7500;
 
 var server = http.createServer(function (request, response){
 	response.statusCode = 503;
-	response.end (randomMean + ' \n Path Hit: ' + request.url);
+	var randomMean = negatives[Math.floor(negatives.length * Math.random())];
+	response.end (
+
+
+		(randomMean + ' \n Path Hit: ' + request.url));
 
 });
 
