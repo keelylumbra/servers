121fe70eda744e405e0dba7ef0c6feabf799b59e
Keely <keelylumbra@knights.ucf.edu>
2016-09-17 10:51:40 -0400

    working

--- server7000.js
+++ server7000.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var PORT = 7000;
 
+
+
+
+var positives = ["That boob job almost looks real!", "I bet you go to the dentist more than bob does", "You dont look like you ate at golden corral last night!"]
+var negatives = ["You got hit with the ugly stick real hard.", "I didnt realize that wasn't a mask!", "Whose your plastic surgeon? I just want to make sure, I don't use him."]
+
+
+var randomNice = positives[Math.floor(positives.length * Math.random())];
+var randomMean = negatives[Math.floor(negatives.length * Math.random())];
+
 var server = http.createServer(function (request, response){
 	response.statusCode = 503;
-	response.end ('You are gawgeous! Path Hit: ' + request.url);
+	response.end (randomNice + '\n Path Hit: ' + request.url);
 
 });
 
@@ -18,7 +28,7 @@ var PORT2 = 7500;
 
 var server = http.createServer(function (request, response){
 	response.statusCode = 503;
-	response.end ('Your butts getting big. Path Hit: ' + request.url);
+	response.end (randomMean + ' \n Path Hit: ' + request.url);
 
 });
 
